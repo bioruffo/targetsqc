d829fbfa24a3cff5f7fb5cfabe971b9ca2c3c15e
Andressa Valengo <valengoandressa@gmail.com>
2017-11-09 09:21:32 -0200

    added support for Galaxy

--- targetsqc.py
+++ targetsqc.py
@@ -9,18 +9,69 @@ Created on Tue Feb 21 14:39:58 2017
 
 # TODO REMEMBER CFTR-37!! The Ion Proton / PGM will trim primer sequences
 # for variant analysis, but the BAM still contain these ends!! Don't count them!!
+# TIP: just employ TVC post-processed BAM
 
 
 import pandas as pd
 import pysam
 import csv
+import argparse
 from bisect import bisect
-from configparser import ConfigParser
 from collections import defaultdict
+from utils import createdir
+
+
+def parse_cmd():
+    '''
+    parsing params from command line
+    '''
+    parser = argparse.ArgumentParser(description='A coverage analysis tool' +
+    'data obtained from amplicon-based exome sequencing')
+    parser.add_argument('-v', '--version', action='version',
+                        version='1.0.0b1')
+    parser.add_argument('-b', '--bamfile', type=str, required=True,
+                        help='alignment filename (BAM format)')
+    parser.add_argument('-t', '--target', type=str, required=True,
+                        help='ampliseq targets filename (BED format)')
+    parser.add_argument('-w', '--wannovar', type=str, required=True,
+                        help='variant annotation filename (CSV format)')
+    parser.add_argument('-a', '--annotation', type=str, required=True,
+                        help='genome annotation file (GFF3 format)')
+    parser.add_argument('-g', '--genelist', type=str, required=True,
+                        help='list of genes of interest (text file).' +
+                        ' One gene per line')
+    parser.add_argument('-p', '--platform', type=str,
+                        nargs='?', default='Proton',
+                        choices=set(('Proton', 'PGM')),
+                        help='sequencing platform')
+    parser.add_argument('-s', '--select', type=bool,
+                        nargs='?', default=True,
+                        choices=set((True, False)),
+                        help='select variants that are within genelist?')
+    parser.add_argument('-q', '--quality', type=int,
+                        nargs='?', default=1,
+                        choices=set((0, 1, 2, 3, 4)))
+    parser.add_argument('-m', '--min-mapping-qv', type=int,
+                        nargs='?', default=4)
+    parser.add_argument('-f', '--min-failed-size', type=int,
+                        nargs='?', default=2,
+                        help='ignore 1-bp dels')
+    parser.add_argument('-u', '--over', type=int,
+                        nargs='?', default=10,
+                        help='add this many nucleotides on each side of exons' +
+                        ' and CDS')
+    parser.add_argument('-o', '--outdir', type=str,
+                        nargs='?', default='targetsqc_output',
+                        help='output dir name')
+    parser.add_argument('-n', '--sample-name', type=str,
+                        nargs='?', default='sample',
+                        help='sample identifier')
+
+    opts = parser.parse_args()
+    return opts
 
 
 class Genedata(object):
-    # genes = Genedata(config.genes, config.bed, config.annotation, over, genesbed)
     def __init__(self, genesfile, bedfile, annot_genes, over, bedout):
         # Load desired gene names from .txt file
         # self.genes_ex = user-declared gene names to search the exome .bed file
@@ -37,7 +88,7 @@ class Genedata(object):
         self.transcripts, self.notfound_transc = self.load_transcripts(annot_genes)
         self.genes_ann = sorted(set(self.genes_ann).difference(self.notfound_transc))
         '''
-        self.data[gene] = {'chr': 'chr17', 'CDS': [], 'exon': [], 
+        self.data[gene] = {'chr': 'chr17', 'CDS': [], 'exon': [],
                            "5'-splice": [], "3'-splice": [], 'missingCDS': []}
         '''
         self.data = dict([(gene, dict()) for gene in self.genes_ex])
@@ -45,7 +96,6 @@ class Genedata(object):
         for gene in self.genes_ex:
             self.populate_data(gene, over)
         self.write_gene_bed(bedout)
-        
 
     def load_genes(self, genesfile):
         with open(genesfile, "r") as f:
@@ -69,27 +119,24 @@ class Genedata(object):
         print("{} valid lines read.".format(linesnum))
         return geneslist, bridgedict
 
-
     def bridge(self, gene):
         return self.bridgedict.get(gene, gene)
-
 
     def load_amplicons(self, bedfile):
         print("Reading amplicons from bed file...")
         amplicons = pd.read_csv(bedfile, sep='\t', header=None, skiprows=1)
-        
-        amplicons['gene'] = amplicons[7].str.extract("GENE_ID=(\w*?);", 
+
+        amplicons['gene'] = amplicons[7].str.extract("GENE_ID=(\w*?);",
                  expand=False)
         amplicons = amplicons[[0, 1, 2, 3, 'gene']]
         amplicons = amplicons[amplicons['gene'].isin(self.genes_ex)]
-        
+
         notfound_bed = sorted(set(self.genes_ex).difference(
                 set(amplicons['gene'])))
         if notfound_bed:
             print("I couldn't locate these {} genes within the exome panel" \
                   "(see report):\n{}".format(len(notfound_bed), ', '.join(notfound_bed)))
         return amplicons, notfound_bed
-
 
     def load_transcripts(self, annot_genes):
         print("Reading transcript CDSs from genome annotation...")
@@ -104,21 +151,20 @@ class Genedata(object):
         transcripts = transcripts.drop_duplicates()
         transcripts['gene'] = transcripts[8].str.extract("\;gene=(\w*?)\;",
                 expand=False)
-        
+
         transcripts = transcripts[transcripts['gene'].isin(self.genes_ann)]
-        
+
         notfound_transc = set(self.genes_ann).difference(set(transcripts['gene']))
         if notfound_transc:
             print("I couldn't locate these {} genes within the transcripts file" \
                   "(see report):\n{}".format(len(notfound_transc), ', '.join( \
                    sorted(list(notfound_transc)))))
         return transcripts, notfound_transc
-    
 
     def get_annot_genenames(self, annot_genes):
         with open(annot_genes, 'r') as f:
             genes = pd.read_csv(f, delimiter='\t', comment='#', header=None, usecols=[0, 2, 8])
-        
+
         genes = genes[genes[2]=='gene']
         genes = genes[genes[0].str.startswith("NC_000")]
         genes = genes[~genes[8].str.contains("pseudo=true")]
@@ -126,7 +172,7 @@ class Genedata(object):
         genes['synonyms'] = genes[8].str.extract(';gene_synonym=(.*?);', expand=False)
         # some genes are still duplicated: tRNA, and a few genes from chromosomes x and y
         # genes[genes.duplicated('name',keep=False)]
-        
+
         # Take all genes with synonyms (they're very few)
         syns = genes[~genes['synonyms'].isnull()][['name', 'synonyms']].to_dict(orient='split')['data']
         names = [s[0] for s in syns]
@@ -137,7 +183,6 @@ class Genedata(object):
                     synonyms[synonym.upper()].append(name.upper())
         return names, synonyms
 
-    
     def populate_data(self, gene, over):
         # print("Populating data for gene: {}".format(gene))
         self.data[gene]['chr'] = self.amplicons[self.amplicons['gene'] == gene].iloc[0,0]
@@ -155,7 +200,7 @@ class Genedata(object):
         self.data[gene]['missingCDS'] = compare_tuplelists(self.data[gene]['CDS'],
                  set_boundaries(nttuple(self.amplicons[self.amplicons['gene'] == gene], [1, 2]),
                                 over=0))
-    
+
     def write_gene_bed(self, bedout):
         print("Writing gene data to", bedout)
         with open(bedout, "w") as f:
@@ -167,9 +212,8 @@ class Genedata(object):
                                                        item[1],
                                                        category,
                                                        gene))
-        
-    
-    
+
+
 class NGSData(object):
     def __init__(self, wannovarfile, bamfile, genes):
         self.genes = genes
@@ -177,7 +221,7 @@ class NGSData(object):
         print("Reading coverage data from:", str(bamfile))
         self.bam = pysam.AlignmentFile(bamfile, "rb")
 
-    
+
     def select(self, output = "selector_output.csv"):
         print("Loading data from:", str(self.wannovar))
         self.data = pd.read_csv(self.wannovar)
@@ -187,8 +231,8 @@ class NGSData(object):
                                    for genes in self.data["Gene.refgene"]]]
         print("Saving variants from selected genes to:", output)
         self.selected.to_csv(output, index=False)
-        
-        
+
+
     def analyze_coverage(self, gene, run_params):
         min_mapping_qv, min_coverage, min_cov_each_strand, strand_bias, \
                 min_failed_size, over = run_params
@@ -237,20 +281,20 @@ class NGSData(object):
                                                   sorted(failedreasons), sorted(failedwhere)])
                             if failedlist[-1][4] < min_failed_size:
                                 failedlist.pop(-1)
-                                
+
                         failedchrom = None
                         failedreasons = set()
                         failedwhere = set()
                     covlist.append(rowdata)
             # closing last pileup region, if open
             if failedchrom:
-                failedlist[-1].extend([min(location, stop), 
+                failedlist[-1].extend([min(location, stop),
                                        (min(location, stop) - failedlist[-1][-1] + 1),
                                        sorted(failedreasons), sorted(failedwhere)])
                 if failedlist[-1][4] < min_failed_size:
                     failedlist.pop(-1)
         return covlist, failedlist
-    
+
     def get_where(self, location, flattened):
         where = []
         for item in ["CDS", "exon"]:
@@ -265,24 +309,7 @@ class NGSData(object):
         return where
 
 
-
-
-class Config(object):
-    def __init__(self, configfile = "selector.cfg", section = "FILES"):
-        parser = ConfigParser()
-        parser.read(configfile)
-        try:
-            self.sectiondata = parser[section]
-        except KeyError as e:
-            print("\n***\nERROR: configuration file invalid, or not found.\n***\n")
-            raise e
-    
-    def __getattr__(self, attr):
-        return self.sectiondata.get(attr, None)
-
-
-
-def report(config, run_params, genes, data,
+def report(bam, run_params, genes, data,
            reportfile = "selector_report.tsv",
            failedbed = "selector_failed.bed"):
     reportfile = reportfile
@@ -304,23 +331,23 @@ def report(config, run_params, genes, data,
     #failed = coverage[coverage[5] == False]
     if failedlist:
         print("Some genes had areas of low coverage (see report).")
-    
+
     if missing:
         print("Some genes had CDS regions without a designed amplicon (see report).")
-        
+
     print("Writing report...{}".format(["", " THERE ARE WARNINGS!"][genes.notfound_bed != [] \
                                                                     or failedlist != [] \
                                                                     or missing != []]))
     with open(reportfile, 'w') as r:
         with open(failedbed, 'w') as f:
-            r.write("Analyzed file:\t{}\n\n".format(config.bam))
+            r.write("Analyzed file:\t{}\n\n".format(bam))
 
             r.write("Parameters used:\n\t{}\n\n".format("\n\t".join(["Minimum coverage\t" + str(min_coverage),
                         "Minimum cov. each strand\t" + str(min_cov_each_strand),
                         "Maximum strand bias\t" + str(strand_bias),
                         "Minimum mapping QV\t"+str(min_mapping_qv),
                         "Minimum failed region size\t"+str(min_failed_size)])))
-    
+
             r.write("Genes of interest:\nExome name\tAnnotation name\tExome check\tAnnotation check\n")
             for gene in genes.genes_ex_input:
                 r.write('\t'.join([gene,
@@ -328,7 +355,7 @@ def report(config, run_params, genes, data,
                                    ["", "Exome: not found"][gene in genes.notfound_bed],
                                    ["", "Annotation: not found", ""][gene in genes.notfound_transc]]) \
                         + '\n')
-            
+
             if missing:
                 r.write("WARNING:\nThese CDS regions had no amplicons:\n")
                 r.write("\tGene\tChrom\tStart\tEnd\tExtension\n")
@@ -339,7 +366,7 @@ def report(config, run_params, genes, data,
                 r.write('\n')
             else:
                 r.write("All CDS were represented by amplicons.\n\n")
-            
+
             if failedlist:
                 r.write("WARNING:\nSome amplicons had low coverage:\n\n")
                 r.write("\tGene\tChrom\tStart\tEnd\tExtension\tReason\tLocation\n")
@@ -347,10 +374,9 @@ def report(config, run_params, genes, data,
                     n, c, s, e, ex, re, *_,  = item
                     r.write(((7*'\t{}')+'\n').format(*item[:-2], ', '.join(item[-2]), ', '.join(item[-1])))
                     f.write("{}\t{}\t{}\t{}\t{}\n".format(c, s, e, '-'.join(re), n))
-                    
+
             else:
                 r.write("All positions had sufficient coverage.")
-    
 
 
 def set_boundaries(tpls, over):
@@ -376,7 +402,7 @@ def nttuple(dataframe, columns, minusone = False):
     col1, col2 = columns
     return [(int(tpl[col1] - int(minusone)), int(tpl[col2])) \
             for idx, tpl in dataframe[columns].iterrows()]
-    
+
 
 def flatten(tuplelist):
     return [item for tpl in tuplelist for item in tpl]
@@ -417,36 +443,36 @@ def compare_tuplelists(query, reference):
             external.extend(newext)
     return external
 
-        
 
 def main():
 
-    hotspot, indel, mnp, snp, custom = 0, 1, 2, 3, 4
-    
-    suffix = "test"
-    platform = "Proton"
-    filter_quality = indel
-    min_mapping_qv = 4
-    min_failed_size = 2 # ignore 1-bp dels
-    over = 10 # add this many nucleotides on each side of exons and CDS
-    configfile = "/home/roberto/Dropbox/Scripts/Selector/exome.cfg"
-    bam = ""
-    wannovar = ""
-    select = True
-    
+    opts = parse_cmd()
+    # hotspot, indel, mnp, snp, custom = 0, 1, 2, 3, 4
+    suffix = opts.sample_name
+    outdir = opts.outdir
+    platform = opts.platform
+    filter_quality = opts.quality
+    min_mapping_qv = opts.min_mapping_qv
+    min_failed_size = opts.min_failed_size # ignore 1-bp dels
+    over = opts.over # add this many nucleotides on each side of exons and CDS
+    bam = opts.bamfile
+    bed = opts.target
+    wannovar = opts.wannovar
+    annotation = opts.annotation
+    genes = opts.genelist
+    select = opts.select
+
+    createdir(outdir)
     # Output file names
-    reportfile = "selector_report_{}.tsv".format(suffix)
-    selectfile = "selector_output_{}.csv".format(suffix)
-    genesbed = "selector_genes_{}.bed".format(suffix)
-    failedbed = "selector_failed_{}.bed".format(suffix)
+    reportfile = "{}/selector_report_{}.tsv".format(outdir, suffix)
+    selectfile = "{}/selector_output_{}.csv".format(outdir, suffix)
+    genesbed = "{}/selector_genes_{}.bed".format(outdir, suffix)
+    failedbed = "{}/selector_failed_{}.bed".format(outdir, suffix)
 
     custom_min_coverage = 6
     custom_min_cov_each_strand = 3
     custom_strand_bias = 0.95
-    
-    
 
-    
     if platform == "Proton":
         # Yeah against PEP8, shoot me
         min_coverage =        [6,   10,   5,    5,    custom_min_coverage][filter_quality]
@@ -461,33 +487,25 @@ def main():
     else:
         print("Unknown platform", platform)
         return 1
-    
-
-
-    config = Config(configfile)
-    if bam:
-        config.bam = bam
-    if wannovar:
-        config.wannovar = wannovar
 
     run_params = [min_mapping_qv, min_coverage, min_cov_each_strand,
                   strand_bias, min_failed_size, over]
-    
+
     if not 'oldgenes' in globals():
-        genes = Genedata(config.genes, config.bed, config.annotation, over, genesbed)
+        genes = Genedata(genes, bed, annotation, over, genesbed)
     else:
         genes = oldgenes
         print("Using previously calculated `genes` data.")
-    
-    data = NGSData(config.wannovar, config.bam, genes)
 
-    report(config, run_params, genes, data, reportfile) # TODO add "over"
+    data = NGSData(wannovar, bam, genes)
+
+    report(bam, run_params, genes, data, reportfile, failedbed) # TODO add "over"
     if select:
         data.select(output=selectfile)
-    
+
     print("End.")
     return genes
-        
+
 
 if __name__ == '__main__':
     oldgenes = main()
